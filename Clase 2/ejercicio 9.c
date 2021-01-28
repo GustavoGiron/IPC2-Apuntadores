73222f1613663f1e518c0266ee9d0b37dc57f3c4
Gustavo Giron <ggironarriolaa@gmail.com>
2021-01-28 08:52:49 -0600

    Create ejercicio 9.c

---
+++ Clase 2/ejercicio 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main() {
+	
+	char c = 'z';
+	char *pc = &c;
+	char **ppc = &pc;
+	char ***pppc = &ppc;
+	***pppc = 'm';
+
+	printf("El ultimo valor del ultimo apuntador es: %c", ***pppc);
+	
+	return 0;
+}
